746e5606fabb4fc4239c10219b1f70fb35c4089b
StackSaga <adiosy@stacksaga.com>
2024-07-07 17:24:35 +0200

    Introducing domain layer

---
+++ Domain/Entities/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public abstract class Animal
+    {
+        public string Name { get; set; }
+        public abstract double FoodConsumption { get; }
+    }
+}
---
+++ Domain/Entities/Carnivore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Carnivore : Animal
+    {
+        public override double FoodConsumption => 3.0;
+    }
+}
